60d373a80baf4a6ea297ed1fa9b044e4f0ee0fb1
Isaac Karth <isaac@isaackarth.com>
2020-06-18 16:34:02 -0700

    fixed backgrounds

--- rom_generator/generator.py
+++ rom_generator/generator.py
@@ -42,6 +42,12 @@ def makeElement():
   element["id"] = str(uuid.uuid4())
   return element.copy()
 
+def makeMusic(name, filename):
+  element = makeElement()
+  element["name"] = name
+  element["filename"] = filename
+  element["_v"] = int(round(time.time() * 1000.0))
+  return element
 
 def makeSpriteSheet(name, frames, type, filename):
   element = makeElement()
@@ -99,18 +105,28 @@ def write_project_to_disk(gb_project, filename="test.gbsproj", output_path="../g
   with open(f"{output_path}{filename}", "w") as wfile:
     wfile.write(generated_project)
 
-
+### Run the generator
 if __name__ == '__main__':
+  # Set up a barebones project
   project = types.SimpleNamespace(**base_gb_project)
   project.settings = default_project_settings.copy()
+
+  # Create sprite sheets
   player_sprite_sheet = makeSpriteSheet("actor_animated", 6, "actor_animated", "actor_animated.png")
   project.spriteSheets.append(player_sprite_sheet)
   a_rock_sprite = makeSpriteSheet("rock", 1, "static", "rock.png")
   project.spriteSheets.append(a_rock_sprite)
+
+  # Add a background image
   default_bkg = makeBackground("placeholder", "placeholder.png", 20, 18, 160, 144)
+  project.backgrounds.append(default_bkg)
+
+  # Create a scene
   a_scene = makeScene("Scene 0", default_bkg["id"], 20, 18, 228, 172)
-  a_scene["actors"].append({"id": "9261decb-55c1-4ab9-ae40-801b1a6a93b8"})
+  # Create an actor
   example_rock = makeActor(a_rock_sprite["id"], 5, 5)
   a_scene["actors"].append(example_rock)
+  # Add scene to project
   project.scenes.append(a_scene)
+
   write_project_to_disk(project)

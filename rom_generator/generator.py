2dc7005bc5264f57e0bb180157464e6a1ed22041
Isaac Karth <isaac@isaackarth.com>
2020-06-18 16:11:43 -0700

    adding actors

--- rom_generator/generator.py
+++ rom_generator/generator.py
@@ -1,5 +1,7 @@
 import json
 import types
+import uuid
+import time
 from pathlib import Path
 
 
@@ -33,10 +35,52 @@ default_project_settings = {
         "playerSpriteSheetId": "581d34d0-9591-4e6e-a609-1d94f203b0cd"
     }
 
+### Create a basic GBS element, with a unique ID
+
 def makeElement():
   element = {}
   element["id"] = str(uuid.uuid4())
   return element.copy()
+
+
+def makeSpriteSheet(name, frames, type, filename):
+  element = makeElement()
+  element["name"] = name
+  element["frames"] = frames
+  element["type"] = type
+  element["filename"] = filename
+  element["_v"] = int(round(time.time() * 1000.0)) # set creation time (for versioning?)
+  return element
+
+def makeBackground(name, filename, width, height, imageWidth, imageHeight):
+  element = makeElement()
+  element["name"] = name
+  element["width"] = width
+  element["height"] = height
+  element["imageWidth"] = imageWidth
+  element["imageHeight"] = imageHeight
+  element["filename"] = filename
+  element["_v"] = int(round(time.time() * 1000.0))
+  return element
+
+def makeScene(name, background, width, height, x, y, collisions=[], actors=[], triggers=[]):
+  element = makeElement()
+  element["name"] = name
+  element["backgroundId"] = background
+  element["width"] = width
+  element["height"] = height
+  element["x"] = x
+  element["y"] = y
+  element["collisions"] = collisions
+  element["actors"] = actors
+  element["triggers"] = triggers
+  return element
+
+
+
+
+
+### Writing the project to disk
 
 def write_project_to_disk(gb_project, filename="test.gbsproj", output_path="../gbprojects/projects/"):
   generated_project = json.dumps(gb_project.__dict__, indent=4)
@@ -49,4 +93,9 @@ def write_project_to_disk(gb_project, filename="test.gbsproj", output_path="../g
 if __name__ == '__main__':
   project = types.SimpleNamespace(**base_gb_project)
   project.settings = default_project_settings.copy()
+  a_rock = makeSpriteSheet("rock", 1, "static", "rock.png")
+  project.spriteSheets.append(a_rock)
+  default_bkg = makeBackground("placeholder", "placeholder.png", 20, 18, 160, 144)
+  a_scene = makeScene("Scene 0", default_bkg["id"], 20, 18, 228, 172)
+  a_scene["actors"].append(a_rock)
   write_project_to_disk(project)

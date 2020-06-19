cb44830f76ddf54410d49fdbe10659f288ed0e5f
Isaac Karth <isaac@isaackarth.com>
2020-06-18 18:06:08 -0700

    music and stuff

--- rom_generator/generator.py
+++ rom_generator/generator.py
@@ -132,6 +132,9 @@ if __name__ == '__main__':
   # Add scene to project
   project.scenes.append(a_scene)
 
+  # Add some music
+  project.music.append(makeMusic("template", "template.mod"))
+
   # Set the starting scene and player sprite
   project.settings["startSceneId"] = a_scene["id"]
   project.settings["playerSpriteSheetId"] = player_sprite_sheet["id"]

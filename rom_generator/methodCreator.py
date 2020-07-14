194cebdddd6ecabcc92c1f0ec181e3a248b39bdd
Aaron Lo <22aaronl@students.harker.org>
2020-07-14 13:53:06 -0700

    Script Generator mostly finished

--- rom_generator/methodCreator.py
+++ rom_generator/methodCreator.py
@@ -1,5 +1,53 @@
 from scripting import getScript
 
+''' Current issues in this generator
+
+-If given argument is a keyword, generator does not detect this (actorPush())
+-If the method has children(specified in the gbsproj file) but does not have __collapseElse, the method will
+    need to be manually updated 
+    ie() EventLoop(), EventGroup(), EventSetInputScript(), EventCallCustomEvent()
+
+REPLACE THIS TO LOOP
+def loop(loopCommand):
+    element = makeElement()
+    element["commands"] = "EVENT_LOOP"
+    element["args"] = {
+    }
+    loopCommand.add(end())
+    element["children"] = {
+        "true": loopCommand
+    }
+    return element
+
+REPLACE THIS TO GROUP
+def group(groupCommands):
+    element = makeElement()
+    element["commands"] = "EVENT_GROUP"
+    element["args"] = {
+    }
+    groupCommands.add(end())
+    element["children"] = {
+        "true": groupCommands
+    }
+    return element
+
+REPLACE THIS TO SETINPUTSCRIPT
+def setInputScript(input = "b",scripts=[]):
+    element = makeElement()
+    element["commands"] = "EVENT_SET_INPUT_SCRIPT"
+    element["args"] = {
+        "input": input,
+    }
+    scripts.add(end())
+    element["children"] = {
+        "true": scripts
+    }
+    return element
+
+I DON'T UNDERSTAND CALL CUSTOM EVENTS SO YOURE GOING TO HAVE TO 
+    DO THIS YOURSELF
+'''
+
 scriptCommands = getScript()
 methods = list(scriptCommands.keys())
 spacing = "    "
@@ -12,10 +60,16 @@ for i in methods:
             else:
                 n += i[j].lower()
     n += "("
+    f = False
     l = list(scriptCommands[i].keys())
     for k in l:
+        if str(k) == "__collapseElse":
+            f = True
         n+= k + " = \"" + str(scriptCommands[i][k]) + "\", "
-    if len(l) > 0:
+    
+    if f:
+       n += "trueCommands = [], falseCommands = []" 
+    elif len(l) > 0:
         n = n[0:len(n) - 2]
     n += "):"
     print(n)
@@ -29,4 +83,18 @@ for i in methods:
         n = spacing + spacing + "\"" + k + "\": " + k + ","
         print(n)
     print(spacing + "}")
+
+    if f:
+        n = spacing + "trueCommands.add(end())"
+        print(n)
+        n = spacing + "falseCommands.add(end())"
+        print(n)
+        n = spacing + "element[\"children\"] = {"
+        print(n)
+        n = spacing + spacing + "\"true\": trueCommands,"
+        print(n)
+        n = spacing + spacing + "\"false\": falseCommands"
+        print(n)
+        print(spacing + "}")
     print(spacing + "return element")
+    print()

7f2db908515654f6afa9b6eef5ddefd667c17877
Isaac Karth <isaackarth@gmail.com>
2020-07-23 13:36:17 -0700

    added main script for dev/test purposes

--- rom_generator/scenes/import.py
+++ rom_generator/scenes/import.py
@@ -1,1 +1,23 @@
 ##### Imports scenes from a gbsproj #####
+
+
+def importFromGBS(filename):
+    return
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Import gbsproj file and translate it into generator templates")
+    parser.add_argument('--import', '-i', type=str, help="gbsproj file to import", default="gbs_projects/scene_templates_test_halls.gbsproj")
+    args = parser.parse_args()
+    importFromGBS(project, output_path = args.import)

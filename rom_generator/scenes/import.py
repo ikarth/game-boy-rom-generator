d2e02c154cf0011997cf689462e3689a49ba5fba
Isaac Karth <isaackarth@gmail.com>
2020-07-23 13:43:59 -0700

    started import function

--- rom_generator/scenes/import.py
+++ rom_generator/scenes/import.py
@@ -1,23 +1,19 @@
 ##### Imports scenes from a gbsproj #####
 
+import json
 
 def importFromGBS(filename):
-    return
-
-
-
-
-
-
-
-
-
-
-
+    proj_data = ""
+    with open(filename, "r", encoding="utf-8") as proj_file:
+        proj_data = json.load(proj_file)
+    print(json.dumps(proj_data, sort_keys=True, indent=3))
+    #print(proj_data)
+    breakpoint()
 
 
 if __name__ == '__main__':
+    import argparse
     parser = argparse.ArgumentParser(description="Import gbsproj file and translate it into generator templates")
-    parser.add_argument('--import', '-i', type=str, help="gbsproj file to import", default="gbs_projects/scene_templates_test_halls.gbsproj")
+    parser.add_argument('--import_file', '-i', type=str, help="gbsproj file to import", default="gbs_projects/scene_templates_test_halls.gbsproj")
     args = parser.parse_args()
-    importFromGBS(project, output_path = args.import)
+    importFromGBS(args.import_file)

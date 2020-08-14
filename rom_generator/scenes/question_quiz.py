56b78a5ca11cc3509d76a7c2d6f5909c0f77cc0d
Isaac Karth <isaackarth@gmail.com>
2020-08-14 14:06:44 -0700

    adding intern generators

---
+++ rom_generator/scenes/question_quiz.py
@@ -0,0 +1,204 @@
+# Generated Scene Functions
+# LiminalRooms.py
+
+from rom_generator import generator
+from rom_generator import script_functions as script
+
+def scene_generation():
+    sprite_sheet_data = [
+        generator.makeSpriteSheet('actor.png', name='actor', type='actor', frames=3),
+        generator.makeSpriteSheet('actor_animated.png', name='actor_animated', type='actor_animated', frames=6),
+        generator.makeSpriteSheet('static.png', name='static', type='static', frames=1)]
+    
+    def findSpriteByName(sprite_name):
+        '''
+        Returns first sprite that matches the name given.
+        '''
+        try:
+            return [s for s in sprite_sheet_data if (s['name'] == sprite_name)][0]
+        except:
+            return None
+
+    def getBySceneLabel(scene_label):
+        '''
+        This is mostly here so we can get the matching scene from the original
+        template data. As used here it just grabs the first scene that was made
+        from that template, so if the template is used more than once it won't
+        behave as expected and you should generate a proper relationship instad.
+        '''
+        s_id = generator.getSceneIdByLabel(scene_label)
+        if s_id == None:
+            return '<♔' + scene_label + '♔>'
+        return s_id
+    
+    def scene_gen_halls_07_00001(callback):
+        actor_name_table = {}
+        actor_list = []
+        trigger_00 = generator.makeTrigger('trigger_00', 9, 4, 2, 1)
+        trigger_01 = generator.makeTrigger('trigger_01', 0, 6, 1, 2)
+        trigger_02 = generator.makeTrigger('trigger_02', 9, 17, 2, 1)
+        trigger_03 = generator.makeTrigger('trigger_03', 19, 11, 1, 2)
+        trigger_list = []
+        collision_data_list = [255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 249, 255, 7, 254, 96, 224, 15, 6, 254, 103, 224, 127, 6, 254, 103, 224, 127, 0, 0, 7, 0, 112, 0, 254, 7, 224, 255, 159, 255, 255, 249, 255, 159, 255]
+        gen_scene_bkg = generator.makeBackground("halls_07.png")
+
+        gen_scene_scn = generator.makeScene("_gen_halls_07", gen_scene_bkg, collisions=collision_data_list, actors=actor_list, triggers=trigger_list, scene_label="scene_gen_halls_07_00001")
+
+        def addConnection_00(source_location, source_size, destination_scene_id, destination_location, destination_direction):
+            trigger_00 = generator.makeTrigger('trigger_connection', source_location[0], source_location[1], source_size[0], source_size[1])
+            trigger_00['script'] = [
+                script.switchScene(sceneId=destination_scene_id, x=destination_location[0], y=destination_location[1], direction=destination_direction, fadeSpeed='2'),
+                script.end()
+            ]
+            return trigger_00
+        connection_00 = {'type': 'SLOT_CONNECTION', 'creator': addConnection_00, 'args': { 'exit_location': (9, 5), 'exit_direction': 'down', 'entrance': gen_scene_scn['id'], 'entrance_location': (9, 4), 'entrance_size': (2, 1)  } }
+
+        def addConnection_01(source_location, source_size, destination_scene_id, destination_location, destination_direction):
+            trigger_01 = generator.makeTrigger('trigger_connection', source_location[0], source_location[1], source_size[0], source_size[1])
+            trigger_01['script'] = [
+                script.switchScene(sceneId=destination_scene_id, x=destination_location[0], y=destination_location[1], direction=destination_direction, fadeSpeed='2'),
+                script.end()
+            ]
+            return trigger_01
+        connection_01 = {'type': 'SLOT_CONNECTION', 'creator': addConnection_01, 'args': { 'exit_location': (1, 7), 'exit_direction': 'right', 'entrance': gen_scene_scn['id'], 'entrance_location': (0, 6), 'entrance_size': (1, 2)  } }
+
+        def addConnection_02(source_location, source_size, destination_scene_id, destination_location, destination_direction):
+            trigger_02 = generator.makeTrigger('trigger_connection', source_location[0], source_location[1], source_size[0], source_size[1])
+            trigger_02['script'] = [
+                script.switchScene(sceneId=destination_scene_id, x=destination_location[0], y=destination_location[1], direction=destination_direction, fadeSpeed='2'),
+                script.end()
+            ]
+            return trigger_02
+        connection_02 = {'type': 'SLOT_CONNECTION', 'creator': addConnection_02, 'args': { 'exit_location': (9, 16), 'exit_direction': 'up', 'entrance': gen_scene_scn['id'], 'entrance_location': (9, 17), 'entrance_size': (2, 1)  } }
+
+        def addConnection_03(source_location, source_size, destination_scene_id, destination_location, destination_direction):
+            trigger_03 = generator.makeTrigger('trigger_connection', source_location[0], source_location[1], source_size[0], source_size[1])
+            trigger_03['script'] = [
+                script.switchScene(sceneId=destination_scene_id, x=destination_location[0], y=destination_location[1], direction=destination_direction, fadeSpeed='2'),
+                script.end()
+            ]
+            return trigger_03
+        connection_03 = {'type': 'SLOT_CONNECTION', 'creator': addConnection_03, 'args': { 'exit_location': (17, 12), 'exit_direction': 'left', 'entrance': gen_scene_scn['id'], 'entrance_location': (19, 11), 'entrance_size': (1, 2)  } }
+
+        gen_scene_connections = [connection_00, connection_01, connection_02, connection_03]
+        scene_data = {"scene": gen_scene_scn, "background": gen_scene_bkg, "sprites": [], "connections": gen_scene_connections, "references": [], "tags": []}
+        return scene_data
+
+
+    def scene_gen_halls_06_00002(callback):
+        actor_name_table = {}
+        actor_list = []
+        trigger_00 = generator.makeTrigger('trigger_00', 2, 9, 2, 1)
+        trigger_01 = generator.makeTrigger('trigger_01', 1, 15, 2, 1)
+        trigger_02 = generator.makeTrigger('trigger_02', 8, 5, 2, 1)
+        trigger_03 = generator.makeTrigger('trigger_03', 8, 17, 2, 1)
+        trigger_04 = generator.makeTrigger('trigger_04', 19, 15, 1, 2)
+        trigger_list = []
+        collision_data_list = [255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 207, 255, 255, 240, 255, 15, 255, 63, 240, 63, 3, 255, 51, 240, 63, 3, 240, 49, 0, 31, 0, 192, 1, 0, 156, 207, 7, 255, 124, 240, 207, 255]
+        gen_scene_bkg = generator.makeBackground("halls_06.png")
+
+        gen_scene_scn = generator.makeScene("_gen_halls_06", gen_scene_bkg, collisions=collision_data_list, actors=actor_list, triggers=trigger_list, scene_label="scene_gen_halls_06_00002")
+
+        def addConnection_00(source_location, source_size, destination_scene_id, destination_location, destination_direction):
+            trigger_00 = generator.makeTrigger('trigger_connection', source_location[0], source_location[1], source_size[0], source_size[1])
+            trigger_00['script'] = [
+                script.switchScene(sceneId=destination_scene_id, x=destination_location[0], y=destination_location[1], direction=destination_direction, fadeSpeed='2'),
+                script.end()
+            ]
+            return trigger_00
+        connection_00 = {'type': 'SLOT_CONNECTION', 'creator': addConnection_00, 'args': { 'exit_location': (2, 10), 'exit_direction': 'down', 'entrance': gen_scene_scn['id'], 'entrance_location': (2, 9), 'entrance_size': (2, 1)  } }
+
+        def addConnection_01(source_location, source_size, destination_scene_id, destination_location, destination_direction):
+            trigger_01 = generator.makeTrigger('trigger_connection', source_location[0], source_location[1], source_size[0], source_size[1])
+            trigger_01['script'] = [
+                script.switchScene(sceneId=destination_scene_id, x=destination_location[0], y=destination_location[1], direction=destination_direction, fadeSpeed='2'),
+                script.end()
+            ]
+            return trigger_01
+        connection_01 = {'type': 'SLOT_CONNECTION', 'creator': addConnection_01, 'args': { 'exit_location': (1, 14), 'exit_direction': 'up', 'entrance': gen_scene_scn['id'], 'entrance_location': (1, 15), 'entrance_size': (2, 1)  } }
+
+        def addConnection_02(source_location, source_size, destination_scene_id, destination_location, destination_direction):
+            trigger_02 = generator.makeTrigger('trigger_connection', source_location[0], source_location[1], source_size[0], source_size[1])
+            trigger_02['script'] = [
+                script.switchScene(sceneId=destination_scene_id, x=destination_location[0], y=destination_location[1], direction=destination_direction, fadeSpeed='2'),
+                script.end()
+            ]
+            return trigger_02
+        connection_02 = {'type': 'SLOT_CONNECTION', 'creator': addConnection_02, 'args': { 'exit_location': (8, 6), 'exit_direction': 'down', 'entrance': gen_scene_scn['id'], 'entrance_location': (8, 5), 'entrance_size': (2, 1)  } }
+
+        def addConnection_03(source_location, source_size, destination_scene_id, destination_location, destination_direction):
+            trigger_03 = generator.makeTrigger('trigger_connection', source_location[0], source_location[1], source_size[0], source_size[1])
+            trigger_03['script'] = [
+                script.switchScene(sceneId=destination_scene_id, x=destination_location[0], y=destination_location[1], direction=destination_direction, fadeSpeed='2'),
+                script.end()
+            ]
+            return trigger_03
+        connection_03 = {'type': 'SLOT_CONNECTION', 'creator': addConnection_03, 'args': { 'exit_location': (8, 16), 'exit_direction': 'up', 'entrance': gen_scene_scn['id'], 'entrance_location': (8, 17), 'entrance_size': (2, 1)  } }
+
+        def addConnection_04(source_location, source_size, destination_scene_id, destination_location, destination_direction):
+            trigger_04 = generator.makeTrigger('trigger_connection', source_location[0], source_location[1], source_size[0], source_size[1])
+            trigger_04['script'] = [
+                script.switchScene(sceneId=destination_scene_id, x=destination_location[0], y=destination_location[1], direction=destination_direction, fadeSpeed='2'),
+                script.end()
+            ]
+            return trigger_04
+        connection_04 = {'type': 'SLOT_CONNECTION', 'creator': addConnection_04, 'args': { 'exit_location': (17, 16), 'exit_direction': 'left', 'entrance': gen_scene_scn['id'], 'entrance_location': (19, 15), 'entrance_size': (1, 2)  } }
+
+        gen_scene_connections = [connection_00, connection_01, connection_02, connection_03, connection_04]
+        scene_data = {"scene": gen_scene_scn, "background": gen_scene_bkg, "sprites": [], "connections": gen_scene_connections, "references": [], "tags": []}
+        return scene_data
+
+
+    def catalog():
+        """
+        Returns a list of scene functions from this part of the library.
+        """
+        return [scene_gen_halls_07_00001,
+            scene_gen_halls_06_00002]
+
+    return catalog, sprite_sheet_data
+
+
+
+def createExampleProject():
+    """
+    Demonstration of how the scene generators in this file can be used.
+    """
+    project = generator.makeBasicProject()
+
+    # Create sprite sheet for the player sprite
+    player_sprite_sheet = generator.addSpriteSheet(project, "actor_animated.png", "actor_animated", "actor_animated")
+    project.settings["playerSpriteSheetId"] = player_sprite_sheet["id"]
+
+    scene_data_list = []
+    catalog, sprites = scene_generation()
+    for scn_func in catalog():
+        scene_data_list.append(scn_func(None))
+    for element_sprite in sprites:
+        project.spriteSheets.append(element_sprite)
+
+    generator.connectScenesRandomlySymmetric(scene_data_list)
+
+    for sdata in scene_data_list:
+        generator.addSceneData(project, generator.translateReferences(sdata, scene_data_list))
+
+    # Add some music
+    project.music.append(generator.makeMusic("template", "template.mod"))
+
+    # Set the starting scene
+    project.settings["startSceneId"] = project.scenes[0]["id"]
+    project.settings["startX"] = 7
+    project.settings["startY"] = 21
+
+    return project
+
+def runTest(test_dir):
+    generator.initializeGenerator()
+    project = createExampleProject()
+    generator.writeProjectToDisk(project, output_path = test_dir)
+
+# test creating scenes...
+if __name__ == '__main__':
+    destination = "../gbprojects/generated_export_test/"
+    runTest(destination)
+
